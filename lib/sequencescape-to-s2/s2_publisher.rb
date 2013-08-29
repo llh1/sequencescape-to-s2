a4b6457f9fb7615b3e140b689bbbcb424f025abc
Loic <llh1@sanger.ac.uk>
2013-08-29 16:57:56 +0100

    Publish messages

--- lib/sequencescape-to-s2/s2_publisher.rb
+++ lib/sequencescape-to-s2/s2_publisher.rb
@@ -1,7 +1,31 @@
+require 'lims-core/persistence/message_bus'
+
 module SequencescapeToS2
   module S2Publisher
+
+    def self.included(klass)
+      klass.class_eval do
+        include Virtus
+        include Aequitas
+        attribute :bus, Lims::Core::Persistence::MessageBus, :required => true, :writer => :private
+      end
+    end
+      
     def publish(objects)
-      # TODO
+      objects.each do |object|
+        bus.publish(object.to_json, :routing_key => routing_key(object))   
+      end
+    end
+
+    def routing_key(object)
+      "sequencescapetos2.#{object.keys.first}.create"
+    end
+
+    def setup_message_bus(settings)
+      @bus = Lims::Core::Persistence::MessageBus.new(settings).tap do |b|
+        b.set_message_persistence(settings["message_persistence"])
+        b.connect
+      end
     end
   end
 end
