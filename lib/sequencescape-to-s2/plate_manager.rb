4974c6d9cd171854efcfb5ef7d3fa5ec1d6693aa
Loic <llh1@sanger.ac.uk>
2013-08-29 16:06:11 +0100

    Refactoring and add calls to encoder and publisher

--- lib/sequencescape-to-s2/plate_manager.rb
+++ lib/sequencescape-to-s2/plate_manager.rb
@@ -1,5 +1,7 @@
 require 'sequencescape-to-s2/resource_mapper'
 require 'sequencescape-to-s2/s2_updater'
+require 'sequencescape-to-s2/s2_encoder'
+require 'sequencescape-to-s2/s2_publisher'
 require 'sequel/adapters/mysql2'
 
 module SequencescapeToS2
@@ -8,28 +10,33 @@ module SequencescapeToS2
     include Aequitas
     include ResourceMapper 
     include S2Updater
+    include S2Encoder
+    include S2Publisher
 
     attribute :sequencescape_db, Sequel::Mysql2::Database, :required => true, :writer => :private
     attribute :laboratory_store, Lims::Core::Persistence::Sequel::Store, :required => true, :writer => :private
     attribute :management_store, Lims::Core::Persistence::Sequel::Store, :required => true, :writer => :private
 
     def initialize(sequencescape_settings, s2_laboratory_settings, s2_management_settings)
-      sequencescape_db_setup(sequencescape_settings)
-      s2_stores_setup(s2_laboratory_settings, s2_management_settings)
+      setup_sequencescape_db(sequencescape_settings)
+      setup_s2_stores(s2_laboratory_settings, s2_management_settings)
     end
 
     def copy_plate_in_s2(plate_uuid)
       objects = load_plate_objects(plate_uuid)
       create_plate_objects(objects)
+      encoded_objects = encode(objects)            
+      debugger
+      publish(encoded_objects)
     end
 
     private
 
-    def sequencescape_db_setup(settings)
+    def setup_sequencescape_db(settings)
       @sequencescape_db = Sequel.connect(settings)
     end
 
-    def s2_stores_setup(laboratory_settings, management_settings)
+    def setup_s2_stores(laboratory_settings, management_settings)
       @laboratory_store = Lims::Core::Persistence::Sequel::Store.new(Sequel.connect(laboratory_settings))
       @management_store = Lims::Core::Persistence::Sequel::Store.new(Sequel.connect(management_settings))
     end
