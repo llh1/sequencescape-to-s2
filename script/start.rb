ebd3aa195f2ae7c2ae0fce653435c2ac1b4ef6bb
Loic <llh1@sanger.ac.uk>
2013-09-02 10:00:39 +0100

    Load api configuration file

--- script/start.rb
+++ script/start.rb
@@ -7,8 +7,9 @@ module SequencescapeToS2
   s2_laboratory_settings = YAML.load_file(File.join('config','s2_database.yml'))[env]['laboratory-app']
   s2_management_settings = YAML.load_file(File.join('config','s2_database.yml'))[env]['management-app']
   amqp_settings = YAML.load_file(File.join('config','amqp.yml'))[env]
+  api_settings = YAML.load_file(File.join('config','api.yml'))[env]
 
-  manager = PlateManager.new(sequencescape_settings, s2_laboratory_settings, s2_management_settings, amqp_settings)
+  manager = PlateManager.new(sequencescape_settings, s2_laboratory_settings, s2_management_settings, amqp_settings, api_settings)
   manager.copy_plate_in_s2("394b3040-ee23-0130-c2bf-7becfc20e235")
   #manager.copy_plate_in_s2("fb3b5e70-ee22-0130-c2bf-7becfc20e235")
 
