1700ba3a84adae989e917cd571ea5a565039023e
Loic <llh1@sanger.ac.uk>
2013-09-10 11:01:35 +0100

    Hack for encoding resource. Switch sample persistor and skip the labellable stream for management-app

--- lib/sequencescape-to-s2/s2_encoder.rb
+++ lib/sequencescape-to-s2/s2_encoder.rb
@@ -1,5 +1,6 @@
 require 'lims-laboratory-app/laboratory/plate/plate_resource'
 require 'lims-laboratory-app/labels/labellable/labellable_resource'
+require 'lims-management-app/sample/sample_resource'
 require 'lims-api/context'
 
 module SequencescapeToS2
@@ -27,8 +28,16 @@ module SequencescapeToS2
       management_uuids = objects[:management].keys
 
       [].tap do |encoded_resources|
+        self.class.switch_sample_persistor_to(Lims::LaboratoryApp::Laboratory::Sample)
         encoded_resources << encode_for_store(:laboratory, laboratory_uuids)
+
+        # Lims::Core::NO_AUTOLOAD needs to be set to avoid the call to 
+        # labellable_to_stream for a sample. 
+        # @see lims-api/core_resource.rb:50
+        self.class.switch_sample_persistor_to(Lims::ManagementApp::Sample)
+        Lims::Core.const_set("NO_AUTOLOAD", 1) unless Lims::Core.const_defined?("NO_AUTOLOAD")
         encoded_resources << encode_for_store(:management, management_uuids)
+        Lims::Core.send(:remove_const, "NO_AUTOLOAD") if Lims::Core.const_defined?("NO_AUTOLOAD")
       end.flatten
     end
 
