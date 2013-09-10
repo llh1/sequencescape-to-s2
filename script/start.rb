b29187c5e7eb8ec1b2fe76ec22091ebe1788b6b7
Loic <llh1@sanger.ac.uk>
2013-09-10 12:06:57 +0100

    Load all the settings and start the bus client

--- script/start.rb
+++ script/start.rb
@@ -1,17 +1,32 @@
 require 'yaml'
 require 'sequencescape-to-s2'
+require 'logging'
 
 module SequencescapeToS2
   env = "development" 
+
   sequencescape_settings = YAML.load_file(File.join('config','sequencescape_database.yml'))[env]
   s2_laboratory_settings = YAML.load_file(File.join('config','s2_database.yml'))[env]['laboratory-app']
   s2_management_settings = YAML.load_file(File.join('config','s2_database.yml'))[env]['management-app']
-  amqp_settings = YAML.load_file(File.join('config','amqp.yml'))[env]
+
+  reception_amqp_settings = YAML.load_file(File.join('config','amqp.yml'))[env]['message_reception']
+  publication_amqp_settings = YAML.load_file(File.join('config','amqp.yml'))[env]['message_publication']
   api_settings = YAML.load_file(File.join('config','api.yml'))[env]
 
-  manager = PlateManager.new(sequencescape_settings, s2_laboratory_settings, s2_management_settings, amqp_settings, api_settings)
-  manager.copy_plate_in_s2("394b3040-ee23-0130-c2bf-7becfc20e235")
-  #manager.copy_plate_in_s2("fb3b5e70-ee22-0130-c2bf-7becfc20e235")
+  manager = PlateManager.new({
+    :sequencescape_settings => sequencescape_settings, 
+    :s2_laboratory_settings => s2_laboratory_settings, 
+    :s2_management_settings => s2_management_settings, 
+    :reception_amqp_settings => reception_amqp_settings, 
+    :publication_amqp_settings => publication_amqp_settings,
+    :api_settings => api_settings
+  })
 
-  puts "Plate imported in S2."
+  manager.set_logger(Logging::LOGGER)
+
+  Logging::LOGGER.info("Sequencescape to S2 started")
+  manager.start
+  Logging::LOGGER.info("Sequencescape to S2 ended")
+
+  #manager.copy_plate_in_s2("394b3040-ee23-0130-c2bf-7becfc20e235")
 end
